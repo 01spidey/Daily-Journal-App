003922b8a466d98fe5b90ffb5f5783f0add18413
01spidey <01kumaragupta@gmail.com>
2023-02-04 17:52:44 +0530

    The app was completed up to Navigating to 'Write Journal' screen. Has No errors !!

--- app/src/main/java/com/example/journalapp/ViewJournalActivity.kt
+++ app/src/main/java/com/example/journalapp/ViewJournalActivity.kt
@@ -13,9 +13,10 @@ class ViewJournalActivity : AppCompatActivity() {
         val binding = ActivityViewJournalBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val day = intent.getStringExtra("day")
-        val month = intent.getStringExtra("month")
-        val year = intent.getStringExtra("year")
+        val day = intent.getStringExtra("day").toString()
+        val month = intent.getStringExtra("month").toString()
+        val year = intent.getStringExtra("year").toString()
+        val content = binding.content.text.toString()
 
         binding.day.text = day
         binding.monthYear.text = String.format(getString(R.string.month_year), month, year)
@@ -28,6 +29,8 @@ class ViewJournalActivity : AppCompatActivity() {
             intent.putExtra("day", day)
             intent.putExtra("month", month)
             intent.putExtra("year", year)
+            intent.putExtra("from", "ViewJournal")
+            intent.putExtra("content",content)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/journalapp/WriteActivity.kt
+++ app/src/main/java/com/example/journalapp/WriteActivity.kt
@@ -1,11 +1,83 @@
 package com.example.journalapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.journalapp.databinding.ActivityWriteBinding
+import java.time.LocalDate
 
 class WriteActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
+
+        val binding = ActivityWriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val content = binding.content
+        val save = binding.save
+
+        val day:String
+        val month:String
+        val year:String
+        var saved:Boolean = false
+        var count = 0
+
+        val from:String = intent.getStringExtra("from").toString()
+
+        if(from=="null"){
+            Toast.makeText(this, "From Home Fragment !!", Toast.LENGTH_SHORT).show()
+            val date = LocalDate.now()
+            day = date.dayOfMonth.toString()
+            month =date.month.toString()
+            year = date.year.toString()
+        }else{
+            val content_txt:CharSequence = intent.getStringExtra("content").toString()
+            Toast.makeText(this, "From $from !!", Toast.LENGTH_SHORT).show()
+            day = intent.getStringExtra("day").toString()
+            month = intent.getStringExtra("month").toString()
+            year = intent.getStringExtra("year").toString()
+            content.textSize = 17f
+            content.setText(content_txt)
+        }
+
+        binding.day.text = day
+        binding.monthYear.text = String.format(getString(R.string.month_year), month, year)
+
+
+        content.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) { }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(from=="null"){
+                    if (s?.isNotEmpty() == true) content.textSize = 17f
+                    else content.textSize = 40f
+                }
+                else{
+                    content.textSize = 17f
+                }
+
+            }
+        })
+
+        save.setOnClickListener{
+            Toast.makeText(this, "Journal is Saving !!", Toast.LENGTH_SHORT).show()
+            saved = true
+        }
+
+        binding.back.setOnClickListener {
+            count++
+            if(saved || count == 2){
+                finish()
+            }else{
+                Toast.makeText(this, "Changes are not saved!! Press again to discard changes !!",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
