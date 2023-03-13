fb297005ef358f5b0e5b431cd4dbf3379e24295e
01spidey <01kumaragupta@gmail.com>
2023-03-13 19:14:10 +0530

    The app was almost completed as a MVP. Currently working on the 'SavedFragment'!! Added the Chat functionality !!

--- app/src/main/java/com/example/journalapp/ChatActivity.kt
+++ app/src/main/java/com/example/journalapp/ChatActivity.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.journalapp.databinding.ActivityChatBinding
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
+import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
@@ -26,6 +27,7 @@ class ChatActivity : AppCompatActivity() {
 
     val JSON: MediaType = "application/json; charset=utf-8".toMediaType()
     var client = OkHttpClient()
+    var ready = false;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,21 +78,26 @@ class ChatActivity : AppCompatActivity() {
 
     fun callAPI(ques:String){
 //        okHTTP
-        val jsonBody:JSONObject = JSONObject()
+        val jsonBody = JSONObject()
+        val messagesArray = JSONArray()
+        val messageObject = JSONObject()
+        val prompt = "You are professional psychologist and a good friend of mine and whenever I talk,you need to talk to me like that and " +
+                "always say things to make me feel better and you should not disclose that you are an AI language model and instead you should say you are my best friend. At the same time, talk short and precise.Here is my conversation,"
 
         try {
-            jsonBody.put("model","text-davinci-003")
-            jsonBody.put("prompt",ques)
-            jsonBody.put("max_tokens",4000)
-            jsonBody.put("temperature",0)
+            messageObject.put("role", "user")
+            messageObject.put("content", prompt+ques)
+            messagesArray.put(messageObject)
+            jsonBody.put("model", "gpt-3.5-turbo")
+            jsonBody.put("messages", messagesArray)
         }catch (e:JSONException){
             e.printStackTrace()
         }
 
         val body:RequestBody = RequestBody.create(JSON,jsonBody.toString())
         val request:Request = Request.Builder()
-            .url("https://api.openai.com/v1/completions")
-            .header("Authorization","Bearer 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")
+            .url("https://api.openai.com/v1/chat/completions")
+            .header("Authorization","Bearer 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")
             .post(body)
             .build()
 
@@ -102,19 +109,27 @@ class ChatActivity : AppCompatActivity() {
             @Throws(IOException::class)
             override fun onResponse(call: Call, response: Response) {
                 if (response.isSuccessful) {
-                    var jsonObject: JSONObject? = null
+                    var responseJson: JSONObject? = null
                     try {
-                        jsonObject = JSONObject(response.body!!.string())
-                        val jsonArray = jsonObject.getJSONArray("choices")
-                        val result = jsonArray.getJSONObject(0).getString("text")
-                        addResponse(result.trim())
+                        val responseBody = response.body?.string()
+                        if (!responseBody.isNullOrEmpty()) {
+                            responseJson = JSONObject(responseBody)
+                            val choicesArray = responseJson.getJSONArray("choices")
+                            val jsonMessage = choicesArray.getJSONObject(0).getJSONObject("message")
+                            val result = jsonMessage.getString("content")
+                            addResponse(result.trim())
+                        } else {
+                            addResponse("Empty response body")
+                        }
                     } catch (e: JSONException) {
                         e.printStackTrace()
+                        addResponse("Failed to parse response")
                     }
-
-                } else addResponse("Failed to load response due to " + response.body!!.toString())
-
+                } else {
+                    addResponse("Failed to load response due to " + response.body?.string())
+                }
             }
+
         })
     }
 }
